fa81389f87a9147cbca7779412f4ff8a74ddbbe2
Rei Rei <onerei666@gmail.com>
2024-07-09 17:50:38 +0300

    исправление и начало работы над кардАдаптером

--- app/src/main/java/com/example/projectsummerpractice/CardAdapter.kt
+++ app/src/main/java/com/example/projectsummerpractice/CardAdapter.kt
@@ -1,4 +1,6 @@
-package com.example.projectsummerpractice;
+package com.example.projectsummerpractice
+
+package com.example.kartochki
 
 import android.content.Intent
 import android.view.LayoutInflater
@@ -10,11 +12,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.projectsummerpractice.Models.Card
 
-class CardAdapter {private var cardList: MutableList<Card>) : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
+class CardAdapter(private var cardList: MutableList<Card>) : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
+
     private var filteredCardList = cardList
 
-    override fun onCreateViewHolder(parent:ViewGroup,viewType: Int): CardViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_card, parent, attachToRoot:false )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_card, parent, false)
+        return CardViewHolder(view)
     }
-}
-}
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        val card = filteredCardList[position]
+        holder.itemName.text = card.name
+
