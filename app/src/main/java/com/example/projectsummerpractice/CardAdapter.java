4f4153c0162e806857d946e852211d11bb39543e
kennuka <143881735+kennuka@users.noreply.github.com>
2024-07-09 17:33:36 +0300

    Начал писать кардАдаптер

--- app/src/main/java/com/example/projectsummerpractice/CardAdapter.java
+++ app/src/main/java/com/example/projectsummerpractice/CardAdapter.java
@@ -1,4 +1,20 @@
 package com.example.projectsummerpractice;
 
-public class CardAdapter {
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.projectsummerpractice.Models.Card
+
+class CardAdapter {private var cardList: MutableList<Card>) : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
+    private var filteredCardList = cardList
+
+    override fun onCreateViewHolder(parent:ViewGroup,viewType: Int): CardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_card, parent, attachToRoot:false )
+    }
+}
 }
