46a408bafef6ca53cd751b19f763deb4a47b28e2
Rei Rei <onerei666@gmail.com>
2024-07-10 15:53:01 +0300

    создание заготовки для фдфптера для избранного

---
+++ app/src/main/java/com/example/projectsummerpractice/CardLikeAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.projectsummerpractice
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.projectsummerpractice.Models.Card
+
+class CardLikeAdapter {
+}
